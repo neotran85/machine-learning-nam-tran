b369b3b2f7166aa77d3cbae0556e100cb2821ba7
neotran85 <namtran1985vn@gmail.com>
2023-11-07 14:16:09 +0000

    Added image pose detector

--- tech/image_pose_detector.py
+++ tech/image_pose_detector.py
@@ -4,24 +4,23 @@ import tempfile
 from PIL import Image, ImageDraw
 import io
 
-st.title("Pose Detection with YOLOv8")
-
-model = YOLO("yolov8x-pose-p6.pt")
+st.title("AI-powered Pose Detector")
+with st.spinner("Loading the AI model..."):
+    model = YOLO("yolov8x-pose-p6.pt")
 
 # Upload form
-uploaded_file = st.file_uploader("Please upload an image or MP4 video (shorter than 30 seconds):", type=["mp4", "jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Please upload an image with people in it...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Process image
-    if uploaded_file.type in ["image/jpeg", "image/png"]:
-        with st.spinner('Analyzing the image...'):
-            bytes_data = uploaded_file.getvalue()
-            image = Image.open(io.BytesIO(bytes_data))
-            # Perform inference
-            results = model(image)
-            # Show the results
-            for r in results:
-                im_array = r.plot()  # plot a BGR numpy array of predictions
-                im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
-            st.image(im)
-    
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    image = Image.open(io.BytesIO(bytes_data))
+
+    with st.spinner("Analyzing the image..."):
+        # Perform inference
+        results = model(image)
+        # Show the results
+        for r in results:
+            im_array = r.plot()  # plot a BGR numpy array of predictions
+            im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+        st.image(im)
