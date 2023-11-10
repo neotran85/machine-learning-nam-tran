85d9cadee36f1893b65e9d0a815ea5af105d240b
neotran85 <namtran1985vn@gmail.com>
2023-11-10 17:08:43 +0000

    Updated code for extender

--- tech/image_extender.py
+++ tech/image_extender.py
@@ -9,45 +9,38 @@ import io
 # Resize the image to the nearest multiple of 64
 def resize_to_multiple_of_64(width, height):
     # Calculate the new dimensions, rounding down to the nearest multiple of 64
-    new_width = (width // 64) * 64
-    new_height = (height // 64) * 64
+    new_width = int((width // 64) * 64)
+    new_height = int((height // 64) * 64)
     return new_width, new_height
 
 # Calculate new dimensions while maintaining aspect ratio
 def calculate_new_dimensions(width, height, max_dimension=1024):
-    # Determine whether to scale based on width or height by finding out which dimension is larger
+    ratio = width / height
     if width > height:
-        # Calculate scaling factor for width
-        scaling_factor = max_dimension / width
+        temp_width = max_dimension
+        temp_height = temp_width / ratio
     else:
-        # Calculate scaling factor for height
-        scaling_factor = max_dimension / height
-    
-    # Calculate new dimensions based on the scaling factor
-    new_width = int(width * scaling_factor)
-    new_height = int(height * scaling_factor)
-    
-    # Ensure that at least one dimension is exactly 1024 pixels
-    if new_width > new_height:
-        new_width = max_dimension
-    else:
-        new_height = max_dimension
-    
-    return resize_to_multiple_of_64(new_width, new_height)
+        temp_height = max_dimension
+        temp_width = temp_height * ratio
+    return resize_to_multiple_of_64(temp_width, temp_height)
 
 # Function to convert image to base64
-def image_to_base64(image):
+def image_to_base64(image, width, height):
+    new_width, new_height = calculate_new_dimensions(width, height)
+    st.write(f"New width: {new_width}, new height: {new_height}")
+    # Resize the image
+    resized_image = image.resize((new_width, new_height))
+    # Convert the resized image to a byte array
     img_byte_arr = io.BytesIO()
-    image.save(img_byte_arr, format='PNG')  # or 'JPEG' depending on your image
+    resized_image.save(img_byte_arr, format='PNG')  # or 'JPEG' depending on your image
     img_byte_arr = img_byte_arr.getvalue()
-    return base64.b64encode(img_byte_arr).decode('utf-8')
+    return base64.b64encode(img_byte_arr).decode('utf-8'), new_width, new_height
 
 # Define the inpainting function using Stability SDK
-def inpaint_with_getimg_ai(prompt, upload_file, mask_file, original_width, original_height, target_dimension=1024):
+def inpaint_with_getimg_ai(prompt, upload_file, mask_file, new_width, new_height, target_dimension=1024):
     # Generate a random seed
     random_seed = random.randint(0, 2**10 - 1)  # for a 32-bit signed integer
      # Calculate new dimensions while maintaining aspect ratio
-    new_width, new_height = calculate_new_dimensions(original_width, original_height)
     url = "https://api.getimg.ai/v1/stable-diffusion/inpaint"
     payload = {
         "image": upload_file,
@@ -100,22 +93,24 @@ if uploaded_file is not None:
     # Create a new image with the desired dimensions
     new_width = image.width + left + right
     new_height = image.height + top + bottom
-    # Fill with gray color
 
+    # Fill with gray color
     new_image = Image.new("RGB", (new_width, new_height), "gray")
 
     # Paste the original image onto the new background
     new_image.paste(image, (left, top))
+    # Resize new_image
+    new_image = new_image.resize((new_width, new_height))
 
     # Display the extended image
     st.image(new_image, caption='Sample Extended Image.', use_column_width=True)
-    new_image_base64_string = image_to_base64(new_image)
+    new_image_base64_string, temp_width, temp_heigh = image_to_base64(new_image, new_width, new_height)
 
     # Create another new image for the rectangle
     rectangle_image = Image.new("RGB", (new_width, new_height), "white")
     draw = ImageDraw.Draw(rectangle_image)
     draw.rectangle([(left + 20, top + 20), (left + image.width - 20, top + image.height - 20)], fill="black")
-    mask_image_base64_string = image_to_base64(rectangle_image)
+    mask_image_base64_string, temp_width, temp_heigh = image_to_base64(rectangle_image, new_width, new_height)
 
     # Display the rectangle image
     # st.image(rectangle_image, caption='Image with Rectangle.', use_column_width=True)
@@ -125,7 +120,7 @@ if uploaded_file is not None:
     if st.button("Extend the image"):
         with st.spinner("Extending the image..."):
             # prompt = prompt + additional_prompt
-            result_image_base64_string = inpaint_with_getimg_ai(prompt + additional_prompt, new_image_base64_string, mask_image_base64_string, new_width, new_height)
+            result_image_base64_string = inpaint_with_getimg_ai(prompt + additional_prompt, new_image_base64_string, mask_image_base64_string, temp_width, temp_heigh)
             if result_image_base64_string:
                 st.image(result_image_base64_string, caption="Result", use_column_width=True)
             else:
