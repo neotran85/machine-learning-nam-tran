2f29f01634c5f2612b93bc4c22c6418259134f04
neotran85 <namtran1985vn@gmail.com>
2023-11-12 06:13:56 +0000

    Updated code.

--- tech/language_chatbot.py
+++ tech/language_chatbot.py
@@ -2,6 +2,17 @@ import streamlit as st
 from openai import OpenAI
 import consts
 import os
+import tempfile
+
+def show_audio(message): 
+    speech_file_path = tempfile.NamedTemporaryFile(delete=True).name
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input=message
+    )
+    response.stream_to_file(speech_file_path)
+    st.audio(speech_file_path)
 
 # Placeholder for chat messages
 chat_container = st.empty()
@@ -25,8 +36,9 @@ def get_ai_response(prompt):
     try:
         response = client.completions.create(
             model="gpt-3.5-turbo-instruct",
-            prompt=prompt,
-            max_tokens=500,
+            prompt=prompt + ". Make it quite short briefly (maximum 200 words), smart, relevant, straightforward and use table to describe",
+            temperature=0.4,
+            max_tokens=400
         )
         return response.choices[0].text
     except Exception as e:
@@ -44,9 +56,24 @@ user_message = st.chat_input("Type your message...")
 if user_message:
     # Add user message to chat history
     st.session_state.chat_history.append(('user', user_message))
-    # show loading message
+
+    # Temporary loading message
+    loading_message = "AI is writing..."
+    st.session_state.chat_history.append(('AI Assistant', loading_message))
+
+    # Display chat history including the loading message
+    for author, message in st.session_state.chat_history:
+        with st.chat_message(author):
+            st.write(message)
+
+    # Get AI response
     ai_response = get_ai_response(user_message)
-    st.session_state.chat_history.append(('AI Assistant', ai_response))
+
+    # Replace the loading message with the actual response
+    st.session_state.chat_history[-1] = ('AI Assistant', ai_response)
+
+    # Redisplay the chat history with the actual response
+    st.experimental_rerun()
 
 show_chat_history()  # Show the chat history
 
@@ -56,4 +83,5 @@ if len(st.session_state.chat_history) > 0:
         st.session_state.chat_history = []
         show_chat_history()  # Show the empty chat history
         chat_container.empty()  # Clear the chat input box
+        st.experimental_rerun()
 
--- tech/language_speech.py
+++ tech/language_speech.py
@@ -1,19 +1,69 @@
-from pathlib import Path
-from openai import OpenAI
-import os
-import consts
 import streamlit as st
+from openai import OpenAI
+import consts
+import os
 import tempfile
 
+def clear_history():
+  st.session_state.audio_history = []
+  show_audio_history()  # Show the empty chat history
+  chat_container.empty()  # Clear the chat input box
+
+def show_audio(message): 
+    # if message is empty, do not show audio
+    if message:
+      speech_file_path = tempfile.NamedTemporaryFile(delete=True).name
+      response = client.audio.speech.create(
+          model="tts-1",
+          voice=selected_voice,
+          input=message
+      )
+      response.stream_to_file(speech_file_path)
+      st.audio(speech_file_path)
+
+
+# Placeholder for chat messages
+chat_container = st.empty()
 os.environ['OPENAI_API_KEY'] = consts.API_KEY_OPEN_AI
 client = OpenAI()
 
-speech_file_path = tempfile.NamedTemporaryFile(delete=True).name
-response = client.audio.speech.create(
-  model="tts-1",
-  voice="alloy",
-  input="Currently, the name is not shown in the UI but is only set as an accessibility label. For accessibility reasons, you should not use an empty string."
-)
-response.stream_to_file(speech_file_path)
-st.audio(speech_file_path)
+def show_audio_history():
+    for author, message in st.session_state.audio_history:
+        with st.chat_message(author):
+            if author == 'AI Assistant':
+                show_audio(message)
+            else:
+              st.write(message)
+
+# Streamlit UI
+st.title("AI-powered Text To Speech")
+voices = ['alloy', 'echo', 'fable', 'onyx', 'nova', 'shimmer'] 
+selected_voice = st.selectbox("Choose a voice", voices)
+
+
+# Initialize chat history in session state
+if 'audio_history' not in st.session_state:
+    st.session_state.audio_history = []
+    
+# Chat input for user message
+user_message = st.chat_input("Type something to for AI to read...")
+
+if user_message:
+    clear_history()  # Clear the chat history
+    # Add user message to chat history
+    st.session_state.audio_history.append(('user', user_message))
+
+    # Temporary loading message
+    loading_message = "AI is preparing..."
+    st.session_state.audio_history.append(('AI Assistant', loading_message))
+    show_audio_history()  # Show chat history with loading message
+
+    # Replace the loading message with the actual response
+    st.session_state.audio_history[-1] = ('AI Assistant', user_message)
+    # Redisplay the chat history with the actual response
+    st.experimental_rerun()
+
+show_audio_history()  # Show the chat history
+
+
 
