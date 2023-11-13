9ab743a1e9e916c3ca482957980fc9a2d6412aa5
neotran85 <namtran1985vn@gmail.com>
2023-11-13 10:27:25 +0000

    Updated code.

--- tech/image_nsfw.py
+++ tech/image_nsfw.py
@@ -31,7 +31,7 @@ if upload_image is not None:
                     {
                         "role": "user",
                         "content": [
-                            {"type": "text", "text": "Count animals in the image. List them out for every kind."},
+                            {"type": "text", "text": "Check if the image is safe for work."},
                             {
                                 "type": "image_url",
                                 "image_url": {
